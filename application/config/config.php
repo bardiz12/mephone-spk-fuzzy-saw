eec7add5e6b94c12e96a3d4dab0968ca78c987bb
Bard <>
2019-11-05 03:05:32 +0700

    fix session for heroku

--- application/config/config.php
+++ application/config/config.php
@@ -381,13 +381,18 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'mysql';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
+
+if(getenv('CLEARDB_DATABASE_URL')){
+    $config['sess_driver'] = 'database';
+    $config['sess_save_path'] = 'ci_sessions';
+}
 
 /*
 |--------------------------------------------------------------------------
